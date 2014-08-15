3b87d0d3459ba7e108732238cc6adb41cd574f9b
U-NSIGHT\asr <asr@NBILL-ASR.Nsight.com>
2014-08-15 12:16:03 -0500

    First Commit

---
+++ InteractiveFictionGame/World.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveFictionGame
+{
+    class World
+    {
+        private List<WorldRoom> _rooms { get; set; }
+        private String _name { get; set; }
+
+        public World(String name)
+        {
+            this._name = name;
+        }
+    }
+}
